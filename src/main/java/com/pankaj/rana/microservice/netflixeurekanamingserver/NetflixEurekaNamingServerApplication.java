230a7ad5eaa56128f14a45bd66abcad88f7cf60b
pankajrana737 <pankajrana737@gmail.com>
2020-01-06 18:19:25 +0530

    modified:   pom.xml 	modified:   src/main/java/com/pankaj/rana/microservice/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/pankaj/rana/microservice/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/pankaj/rana/microservice/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -2,8 +2,11 @@ package com.pankaj.rana.microservice.netflixeurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
